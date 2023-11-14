095429aae3843f7320d5675dcb8fb6a4c7cbfe46
Silviu Stăncioiu <silviu.stancioiu@gmail.com>
2023-11-14 09:14:25 +0200

    Removed dynamic shadows of objects being picked up.

--- Assets/Scripts/SuperliVR/Picking/PickableObject.cs
+++ Assets/Scripts/SuperliVR/Picking/PickableObject.cs
@@ -2,24 +2,34 @@ using UnityEngine;
 
 namespace SuperliVR.Picking
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody), typeof(MeshRenderer))]
     public class PickableObject : MonoBehaviour
     {
-        private Rigidbody _rigidbody;
+        private Rigidbody    _rigidbody;
+        private MeshRenderer _meshRenderer;
 
-        public void PickUp() =>
+        public void PickUp()
+        {
             _rigidbody.isKinematic = true;
-        
-        public void DropDown() =>
+            _meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        }
+
+        public void DropDown()
+        {
             _rigidbody.isKinematic = false;
+            _meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
 
         public void SetPosition(Vector3 position)
         {
             _rigidbody.position = position;
-            transform.position = position;
+            transform.position  = position;
         }
 
-        private void Awake() =>
-            _rigidbody = GetComponent<Rigidbody>();
+        private void Awake()
+        {
+            _rigidbody    = GetComponent<Rigidbody>();
+            _meshRenderer = GetComponent<MeshRenderer>();
+        }
     }
 }
