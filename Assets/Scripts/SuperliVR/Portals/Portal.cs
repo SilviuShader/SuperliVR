963a3c3104c83b16c4e54ac917cdb1e8eab09a80
Silviu Stăncioiu <silviu.stancioiu@gmail.com>
2024-02-05 18:55:39 +0200

    Portal correct rendering in VR for left eye.

--- Assets/Scripts/SuperliVR/Portals/Portal.cs
+++ Assets/Scripts/SuperliVR/Portals/Portal.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using Utils;
+using Valve.VR;
 
 namespace SuperliVR.Portals
 {
@@ -43,32 +45,71 @@ namespace SuperliVR.Portals
             _renderCamera = renderCameraObject.AddComponent<UnityEngine.Camera>();
             _renderCamera.targetTexture = _renderTexture = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
             _renderTexture.Create();
+
+            if (VRHelper.Instance.VRMode)
+                InitVRCamera();
         }
 
+        private void InitVRCamera()
+        {
+            Valve.VR.EVREye eye;
+            //if (isLeftEye)
+            //{
+                eye = Valve.VR.EVREye.Eye_Left;
+            //}
+            //else
+            //{
+            //    eye = Valve.VR.EVREye.Eye_Right;
+            //}
+            _renderCamera.projectionMatrix = HMDMatrix4x4ToMatrix4x4(SteamVR.instance.hmd.GetProjectionMatrix(eye, _renderCamera.nearClipPlane, _renderCamera.farClipPlane));
+        }
         private void OnDestroy() => _renderTexture.Release();
 
         private void Update()
         {
-            if (Screen.width != _renderTexture.width || Screen.height != _renderTexture.height)
+            var targetWidth = Screen.width;
+            var targetHeight = Screen.height;
+
+            if (VRHelper.Instance.VRMode)
+            {
+                targetWidth  = UnityEngine.XR.XRSettings.eyeTextureWidth;
+                targetHeight = UnityEngine.XR.XRSettings.eyeTextureHeight;
+            }
+
+            if (targetWidth != _renderTexture.width || targetHeight != _renderTexture.height)
             {
                 _renderTexture.Release();
-                _renderCamera.targetTexture = _renderTexture = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+                _renderCamera.targetTexture = _renderTexture = new RenderTexture(targetWidth, targetHeight, 16, RenderTextureFormat.ARGB32);
                 _renderTexture.Create();
             }
 
             _renderSurface.material.SetTexture("_MainTex", _renderTexture);
 
             var worldInOtherPortal = WorldInOtherPortal(_playerCamera);
-
             _renderCamera.transform.FromMatrix(worldInOtherPortal);
+
+            if (VRHelper.Instance.VRMode)
+            {
+                Vector3 eyeOffset = Vector3.zero;
+                //if (isLeftEye)
+                //{
+                    eyeOffset = SteamVR.instance.eyes[0].pos;
+                //}
+                //else
+                //{
+                //    eyeOffset = SteamVR.instance.eyes[1].pos;
+                //}
+
+                _renderCamera.transform.position += _renderCamera.transform.TransformVector(eyeOffset);
+                _renderCamera.transform.localRotation = _renderCamera.transform.localRotation;
+            }
 
             var p = new Plane(-_otherPortal.transform.forward, _otherPortal.transform.position);
             var clipPlaneWorldSpace = new Vector4(p.normal.x, p.normal.y, p.normal.z, p.distance);
             var clipPlaneCameraSpace =
                 Matrix4x4.Transpose(Matrix4x4.Inverse(_renderCamera.worldToCameraMatrix)) * clipPlaneWorldSpace;
-
-            var newMatrix = _renderCamera.CalculateObliqueMatrix(clipPlaneCameraSpace);
-            _renderCamera.projectionMatrix = newMatrix;
+            
+            _renderCamera.projectionMatrix = CalculateObliqueMatrix(_renderCamera.projectionMatrix, clipPlaneCameraSpace);
         }
 
         private Matrix4x4 WorldInOtherPortal(Transform currentWorld)
@@ -77,6 +118,50 @@ namespace SuperliVR.Portals
             var playerCameraInPortal = transform.worldToLocalMatrix * playerCameraWorld;
             var localReflected = Matrix4x4.Rotate(Quaternion.Euler(0.0f, 180.0f, 0.0f)) * playerCameraInPortal;
             return _otherPortal.transform.localToWorldMatrix * localReflected;
+        }
+
+        private Matrix4x4 HMDMatrix4x4ToMatrix4x4(HmdMatrix44_t input)
+        {
+            var m = Matrix4x4.identity;
+
+            m[0, 0] = input.m0;
+            m[0, 1] = input.m1;
+            m[0, 2] = input.m2;
+            m[0, 3] = input.m3;
+
+            m[1, 0] = input.m4;
+            m[1, 1] = input.m5;
+            m[1, 2] = input.m6;
+            m[1, 3] = input.m7;
+
+            m[2, 0] = input.m8;
+            m[2, 1] = input.m9;
+            m[2, 2] = input.m10;
+            m[2, 3] = input.m11;
+
+            m[3, 0] = input.m12;
+            m[3, 1] = input.m13;
+            m[3, 2] = input.m14;
+            m[3, 3] = input.m15;
+
+            return m;
+        }
+
+        private static Matrix4x4 CalculateObliqueMatrix(Matrix4x4 projection, Vector4 clipPlane)
+        {
+            Matrix4x4 obliqueMatrix = projection;
+            Vector4 q = projection.inverse * new Vector4(
+                Math.Sign(clipPlane.x),
+                Math.Sign(clipPlane.y),
+                1.0f,
+                1.0f
+            );
+            Vector4 c = clipPlane * (2.0F / (Vector4.Dot(clipPlane, q)));
+            obliqueMatrix[2] = c.x - projection[3];
+            obliqueMatrix[6] = c.y - projection[7];
+            obliqueMatrix[10] = c.z - projection[11];
+            obliqueMatrix[14] = c.w - projection[15];
+            return obliqueMatrix;
         }
     }
 }
